37197b8d4016cb86015863d4b658a7539f009d78
Qbenedict <q.benedict@okstate.edu>
2020-04-29 18:31:19 -0700

    bug fixes

--- Alumni-Swag-App/src/app/shopping-cart/cart/cart-item/cart-item.component.ts
+++ Alumni-Swag-App/src/app/shopping-cart/cart/cart-item/cart-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
@@ -7,7 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CartItemComponent implements OnInit {
 
- @Input() cartItem:any 
+ @Input() cartItems:any
+
+ 
 
   constructor() { }
 
